658da2fec1e20623f752ca760db3895e044dfb51
Ili <sion9801@gmail.com>
2023-02-12 04:15:10 -0500

    hp bar

--- Tutorial/Assets/Script/Controller.cs
+++ Tutorial/Assets/Script/Controller.cs
@@ -5,17 +5,128 @@ using UnityEngine.UI;
 
 public class Controller : MonoBehaviour
 {
-    public Button[] buttons;
+    public GameObject[] characters;
+
+    private int curCharacterID;
+    private int curTargetID;
+    private int curSkillID;
 
     // Start is called before the first frame update
     void Start()
     {
+      for(int i = 0; i < 10; i += 1)
+      {
+        Material hpInstance = Instantiate(characters[i].GetComponent<MyCharacter>().findObject("HP").GetComponent<Image>().material);
+        Material mpInstance = Instantiate(characters[i].GetComponent<MyCharacter>().findObject("MP").GetComponent<Image>().material);
 
+        characters[i].GetComponent<MyCharacter>().findObject("HP").GetComponent<Image>().material = hpInstance;
+        characters[i].GetComponent<MyCharacter>().findObject("MP").GetComponent<Image>().material = mpInstance;
+
+        characters[i].GetComponent<MyCharacter>().findObject("Image").GetComponent<Button>().onClick.AddListener(
+          delegate
+          {
+            int finalI = i;
+            curTargetID = finalI;
+          }
+        );
+      }
+
+      startLevel1();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void startLevel1()
     {
+      characters[3].SetActive(false);
+      characters[4].SetActive(false);
+      characters[6].SetActive(false);
+      characters[7].SetActive(false);
+      characters[8].SetActive(false);
+      characters[9].SetActive(false);
 
+      initParameterInfo(0, "Tank", 2000, 70, 100, 100, 35);
+      initParameterInfo(1, "Dps", 1200, 100, 200, 30, 40);
+      initParameterInfo(2, "Healer", 1200, 150, 125, 50, 35);
+      initParameterInfo(5, "BOSS", 10000, 500, 300, 25, 80);
+
+      characters[0].GetComponent<MyCharacter>().status.skills = new SkillAbility[]{
+        getSkillInfo(0),
+        getSkillInfo(3)
+      };
+
+      characters[1].GetComponent<MyCharacter>().status.skills = new SkillAbility[]{
+        getSkillInfo(0),
+        getSkillInfo(3)
+      };
+
+      characters[2].GetComponent<MyCharacter>().status.skills = new SkillAbility[]{
+        getSkillInfo(0),
+        getSkillInfo(3)
+      };
+
+      characters[5].GetComponent<MyCharacter>().status.skills = new SkillAbility[]{
+        getSkillInfo(0),
+        getSkillInfo(2)
+      };
+    }
+
+    private void initParameterInfo(int id, string name, int hp, int mp, int atk, int def, int spd)
+    {
+      characters[id].GetComponent<MyCharacter>().parameter = new Parameter();
+      characters[id].GetComponent<MyCharacter>().status = new BattleStatus();
+
+      characters[id].GetComponent<MyCharacter>().parameter.name = name;
+
+      characters[id].GetComponent<MyCharacter>().parameter.hp = hp;
+      characters[id].GetComponent<MyCharacter>().status.curHp = hp;
+      characters[id].GetComponent<MyCharacter>().status.maxHp = hp;
+
+      characters[id].GetComponent<MyCharacter>().parameter.mp = mp;
+      characters[id].GetComponent<MyCharacter>().status.curMp = mp;
+      characters[id].GetComponent<MyCharacter>().status.maxMp = mp;
+
+      characters[id].GetComponent<MyCharacter>().parameter.atk = atk;
+      characters[id].GetComponent<MyCharacter>().status.curAtk = atk;
+      characters[id].GetComponent<MyCharacter>().status.maxAtk = atk;
+
+      characters[id].GetComponent<MyCharacter>().parameter.def = def;
+      characters[id].GetComponent<MyCharacter>().status.curDef = def;
+      characters[id].GetComponent<MyCharacter>().status.maxDef = def;
+
+      characters[id].GetComponent<MyCharacter>().parameter.spd = spd;
+      characters[id].GetComponent<MyCharacter>().status.curSpd = spd;
+      characters[id].GetComponent<MyCharacter>().status.maxSpd = spd;
+
+      characters[id].GetComponent<MyCharacter>().status.buff = new ArrayList();
+    }
+
+    private GameObject findObject(GameObject obj, string name)
+    {
+      for (int i = 0; i < obj.transform.childCount; i += 1)
+      {
+        if(obj.transform.GetChild(i).gameObject.name == name)
+        {
+          return obj.transform.GetChild(i).gameObject;
+        }
+      }
+
+      return null;
+    }
+
+    private void castSkill()
+    {
+        characters[curCharacterID].GetComponent<Skill>().castSkill(curSkillID, characters[curCharacterID], characters[curTargetID]);
+    }
+
+    private SkillAbility getSkillInfo(int id)
+    {
+      switch(id)
+      {
+        case 0: return new SkillAbility(0, "Normal Attack", 0, 0, 2, false);
+        case 1: return new SkillAbility(1, "Taunt", 20, 7, 0, false);
+        case 2: return new SkillAbility(2, "ATK UP", 15, 10, 0, false);
+        case 3: return new SkillAbility(3, "HP Regeneration", 0, 0, 0, true);
+      }
+
+      return null;
     }
 }
--- Tutorial/Assets/Script/MyCharacter.cs
+++ Tutorial/Assets/Script/MyCharacter.cs
@@ -1,36 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MyCharacter : MonoBehaviour
 {
   public Parameter parameter;
   public BattleStatus status;
 
-  private GameObject curTarget;
-  private int curSkillID;
-
-  void Start()
+  void Update()
   {
-    parameter = new Parameter();
-    status = new BattleStatus();
+    if(status != null)
+    {
+      Vector3 hpColor = Vector3.Lerp(new Vector3(1, 0, 0), new Vector3(0, 1, 0), status.curHp / status.maxHp);
+      findObject("HP").GetComponent<Image>().material.SetColor("_color", new Color(hpColor.x, hpColor.y, hpColor.z, 1f));
+      findObject("HP").GetComponent<Image>().material.SetFloat("_percentage", status.curHp / status.maxHp);
+      findObject("MP").GetComponent<Image>().material.SetFloat("_percentage", status.curMp / status.maxMp);
+
+    }
   }
 
-  void castSkill()
+  public GameObject findObject(string name)
   {
-      GetComponent<Skill>().castSkill(curSkillID, gameObject, curTarget);
+    for (int i = 0; i < transform.childCount; i += 1)
+    {
+      if(transform.GetChild(i).gameObject.name == name)
+      {
+        return transform.GetChild(i).gameObject;
+      }
+    }
+
+    return null;
   }
 }
 
 public class Parameter
 {
+  public string name;
   public float hp;
   public float mp;
   public float atk;
   public float def;
   public float spd;
-
-  public int[] skills;
 }
 
 public class BattleStatus
@@ -50,6 +61,7 @@ public class BattleStatus
   public float maxSpd;
   public float curSpd;
 
+  public SkillAbility[] skills;
   public ArrayList buff;
 }
 
@@ -62,5 +74,25 @@ public class Buff
   {
     this.id = id;
     this.remainingTurn = turn;
+  }
+}
+
+public class SkillAbility
+{
+  public int id;
+  public string name;
+  public int mpCost;
+  public int cooldown;
+  public int type;
+  public bool isPassive;
+
+  public SkillAbility(int id, string name, int mpCost, int cooldown, int type, bool isPassive)
+  {
+    this.id = id;
+    this.name = name;
+    this.mpCost = mpCost;
+    this.cooldown = cooldown;
+    this.type = type;
+    this.isPassive = isPassive;
   }
 }
